fc6dd7c1be941905ba71d7df839d3b2021201f8a
maxineparedes <maxineparedes@gmail.com>
2025-04-23 11:49:51 -0700

    created function for is_leap

---
+++ is_leap.R
@@ -0,0 +1,26 @@
+#' is this a leap year
+#'
+#' @param input a number, or a string that contains a number
+#'
+#' @return TRUE or FALSE on whether it is a leap year
+#' 
+#' @export
+#' 
+#' @examples
+#' isleap(1992) # returns TRUE
+#' isleap(2000) # returns TRUE
+#' isleap(1900) # returns FALSE
+#' isleap(2021) # returns FALSE
+
+is_leap <- function(input) {
+  if (input %% 4 == 0) {
+    return(TRUE)
+  } else if (input %% 4 == 0 && input %% 100 == 0) {
+    return(FALSE)
+  } else if (input %% 400 == 0) {
+    return(TRUE)
+  } else {
+    return(FALSE)
+  }
+
+}
